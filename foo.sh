dd8160b5dec4ae6852c3a966510be85bb4f1bf35
unart <ben@untar.org>
2011-03-11 03:29:17 +0100

    a better virun

---
+++ foo.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# foo.sh l | grep -v l
+
+#  foo.sh - 
+#  
+#  Copyright (c) 2011 [:VIM_EVAL:]authorname[:END_EVAL:] <[:VIM_EVAL:]authorsite[:END_EVAL:]>
+#  2011-03-11 / ... | ...
+# 
+set -u
+set -e
+
+echo foo  $1
